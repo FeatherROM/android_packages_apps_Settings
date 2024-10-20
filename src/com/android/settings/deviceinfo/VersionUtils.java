Franxx-OS/packages_apps_Settings

Settings: Append custom version on build display id
Change-Id: I831800ae0a769513b6b0c008ba492cd8334b3b65
Signed-off-by: Karan Parashar <whyredfire@gmail.com>

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -1,0 +1,10 @@
+
+package com.android.settings.deviceinfo;
+
+import android.os.SystemProperties;
+
+public class VersionUtils {
+    public static String getcustomVersion(){
+        return SystemProperties.get("ro.custom.version","");
+    }
+}

FeatherROM/android_packages_apps_Settings

Signed-off-by: Ali Hasan <ahb7671@gmail.com>

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -1,0 +1,10 @@
+
+package com.android.settings.deviceinfo;
+
+import android.os.SystemProperties;
+
+public class VersionUtils {
+    public static String getcustomVersion(){
+        return SystemProperties.get("ro.feather.version","");
+    }
+}